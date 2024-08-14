f1d6734ed4f1bae6196174742c8132cd6cae316b
talha <talha.emir.dogan@gmail.com>
2024-08-14 16:44:33 -0400

    Arthur

---
+++ src/Arthurclass/week08/Athlet.java
@@ -0,0 +1,24 @@
+package Arthurclass.week08;
+
+public class Athlet {
+
+    public static void main(String[] args) {
+
+        /*every lap should be 3 push up
+
+        for (int i = 1; i <= 5; i++) { // outer loop
+            System.out.println("Lap " + i + "is finished." );
+            for (int j = 1; j <= 3; j++) {
+                System.out.println("..... push up is done " +j + "is done." );
+            }
+        }
+
+
+         */
+
+
+
+
+
+    }
+}
