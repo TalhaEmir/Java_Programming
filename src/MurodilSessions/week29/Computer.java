54699546f78ea93574705c8a91dcfd12acafd804
talha <talha.emir.dogan@gmail.com>
2024-08-18 17:05:10 -0400

    .

---
+++ src/MurodilSessions/week29/Computer.java
@@ -0,0 +1,27 @@
+package MurodilSessions.week29;
+/*
+Create a class Computer in package access.a
+        define the instance variables with access modifiers:
+            os - public
+            memory - protected
+            brand - default
+            has wifi card - private
+
+        create a toString
+ */
+public class Computer {
+    public String os;
+    protected int memory;
+    String brand;
+    private boolean hasWifiCard;
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "os='" + os + '\'' +
+                ", memory=" + memory +
+                ", brand='" + brand + '\'' +
+                ", hasWifiCard=" + hasWifiCard +
+                '}';
+    }
+}
